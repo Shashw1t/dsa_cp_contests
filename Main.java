b5a1a5ef655e6db18e5e90ff5c6d2788469718a6
tncts <shashwat.satvik@gmail.com>
2024-06-25 22:09:48 +0530

    added

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
-        
         while (t-- > 0) {
             int n = scanner.nextInt(); 
             int[] p = new int[n];
